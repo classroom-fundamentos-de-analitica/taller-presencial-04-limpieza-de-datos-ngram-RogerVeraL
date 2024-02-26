ffb3df51222911586e1d633936aa529e7b469e09
Roger Vera Londoño <rveral@unal.edu.co>
2024-02-26 11:21:12 -0500

    Update clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -1,54 +1,55 @@
-"""Taller evaluable presencial"""
+    """Taller evaluable presencial"""
 
 import pandas as pd
 
 
 def load_data(input_file):
     """Lea el archivo usando pandas y devuelva un DataFrame"""
-    data = pd.read_csv(input_file, sep="\t")
-    return data
+
 
 def create_key(df, n):
-
     """Cree una nueva columna en el DataFrame que contenga el key de la columna 'text'"""
+
     df = df.copy()
 
-    df["key"] = df["text"]
-
-    df["key"] = (
-        df["key"]
+    # 1. Copie la columna 'text' a la columna 'fingerprint'
+    df["fingerprint"] = df["text"]
+    # Copie la columna 'text' a la columna 'key'
+    # Remueva los espacios en blanco al principio y al final de la cadena
+    # Convierta el texto a minúsculas
+    # Transforme palabras que pueden (o no) contener guiones por su version sin guion.
+    # Remueva puntuación y caracteres de control
+    # Convierta el texto a una lista de tokens
+    df["fingerprint"] = (
+        df["fingerprint"]
         .str.strip()
         .str.lower()
+        .str.replace("-", "")
         .str.translate(str.maketrans("", "", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"))
         .str.split()
-        .str.join(" ")
+        .str.join("")
         .apply(lambda x: [x[i : i + n] for i in range(len(x) - n + 1)])
-        .apply(lambda x: [nltk.PorterStemmer().stem(w) for w in x])
         .apply(lambda x: sorted(set(x)))
         .str.join(" ")
     )
 
-    
+    # Una el texto sin espacios en blanco
+    # Convierta el texto a una lista de n-gramas
+    # Ordene la lista de n-gramas y remueve duplicados
+    # Convierta la lista de ngramas a una cadena
+
     return df
+
 
 def generate_cleaned_column(df):
     """Crea la columna 'cleaned' en el DataFrame"""
 
     df = df.copy()
 
-    # 1. Ordene el dataframe por 'fingerprint' y 'text'
-    df=df.sort_values(by=["key","text"]).copy()
-
-    # 2. Seleccione la primera fila de cada grupo de 'fingerprint'
-    fingerprints=df.groupby("key").first().reset_index()
-
-    # 3.  Cree un diccionario con 'fingerprint' como clave y 'text' como valor
-    fingerprints=fingerprints.set_index("key")["text"].to_dict()
-
-    
-    df["cleaned"]=df["key"].map(fingerprints)
-    # 4. Cree la columna 'cleaned' usando el diccionario
-
+    # Ordene el dataframe por 'key' y 'text'
+    # Seleccione la primera fila de cada grupo de 'key'
+    # Cree un diccionario con 'key' como clave y 'text' como valor
+    # Cree la columna 'cleaned' usando el diccionario
 
     return df
 
