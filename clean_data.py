7718767cfa7d22c6cdac0789408abd7f3474b770
Roger Vera Londoño <rveral@unal.edu.co>
2024-02-26 11:15:45 -0500

    Update clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -9,33 +9,25 @@ def load_data(input_file):
     return data
 
 def create_key(df, n):
+
     """Cree una nueva columna en el DataFrame que contenga el key de la columna 'text'"""
-    
     df = df.copy()
 
-    # 1. Copie la columna 'text' a la columna 'fingerprint'
     df["fingerprint"] = df["text"]
-    # 2. Remueva los espacios en blanco al principio y al final de la cadena
-    df["fingerprint"] = df["fingerprint"].str.strip()
-    # 3. Convierta el texto a minúsculas
-    df["fingerprint"] = df["fingerprint"].str.lower()
-    # 4. Transforme palabras que pueden (o no) contener guiones por su version sin guion.
-    df["fingerprint"] = df["fingerprint"].str.replace("-", "")
-    # 5. Remueva puntuación y caracteres de control
-    df["fingerprint"] = df["fingerprint"].str.translate(str.maketrans("", "", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"))
-    # 6. Convierta el texto a una lista de tokens
-    df["fingerprint"] = df["fingerprint"].str.split()
-    # Una el texto sin espacios en blanco
-    df["fingerprint"] = df["fingerprint"].str.join("")
-    # Convierta el texto a una lista de n-gramas
-    df["fingerprint"] = df["fingerprint"].apply(lambda x: [x[i:i+n] for i in range(len(x)-n+1)])
-    # Ordene la lista de n-gramas y remueve duplicados
-    df["fingerprint"] = df["fingerprint"].apply(lambda x: sorted(set(x)))
-    # 9. Convierta la lista de tokens a una cadena de texto separada por espacios
-    df["fingerprint"] = df["fingerprint"].str.join(" ")
-    # Copie la columna 'text' a la columna 'key'
-    df["key"] = df["fingerprint"]
-    # Convierta la lista de ngramas a una cadena
+
+    df["fingerprint"] = (
+        df["fingerprint"]
+        .str.strip()
+        .str.lower()
+        .str.translate(str.maketrans("", "", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"))
+        .str.split()
+        .str.join(" ")
+        .apply(lambda x: [x[i : i + n] for i in range(len(x) - n + 1)])
+        .apply(lambda x: [nltk.PorterStemmer().stem(w) for w in x])
+        .apply(lambda x: sorted(set(x)))
+        .str.join(" ")
+    )
+
     
     return df
 
